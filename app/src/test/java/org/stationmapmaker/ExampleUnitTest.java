d4474b8319cc5c4b0021aacbece708eae3d4cccd
shawnjobseeker <shawnno1@hotmail.co.uk>
2016-12-30 09:47:29 +0000

    Added color picker

--- app/src/test/java/org/stationmapmaker/ExampleUnitTest.java
+++ app/src/test/java/org/stationmapmaker/ExampleUnitTest.java
@@ -1,6 +1,8 @@
 package org.stationmapmaker;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +11,10 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertEquals(4, 2 + 3);
     }
 }
